1a67849554f0bdb5646672b40e63701222cb6693
Berkay Demir <berkayydmir@gmail.com>
2024-03-27 22:29:47 +0300

    feat : upload progress modified

--- InstagramClone/UploadViewController.swift
+++ InstagramClone/UploadViewController.swift
@@ -6,24 +6,62 @@
 //
 
 import UIKit
+import FirebaseStorage
 
-class UploadViewController: UIViewController {
+class UploadViewController: UIViewController, UIImagePickerControllerDelegate & UINavigationControllerDelegate {
 
+    @IBOutlet weak var imageView: UIImageView!
+    
+    @IBOutlet weak var commentText: UITextField!
     override func viewDidLoad() {
         super.viewDidLoad()
 
-        // Do any additional setup after loading the view.
+        imageView.isUserInteractionEnabled = true
+        let gestureRecognizer = UITapGestureRecognizer(target: self, action: #selector(uploadPhoto))
+        imageView.addGestureRecognizer(gestureRecognizer)
     }
     
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
+    @objc func uploadPhoto () {
+        let imagePicker = UIImagePickerController()
+        imagePicker.sourceType = .photoLibrary
+        imagePicker.delegate = self
+        present(imagePicker, animated: true, completion: nil)
     }
-    */
-
+    
+    func imagePickerController(_ picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [UIImagePickerController.InfoKey : Any]) {
+        if let pickedImage = info[.originalImage] as? UIImage {
+            imageView.image = pickedImage
+        }
+        
+        picker.dismiss(animated: true)
+    }
+    
+    @IBAction func uploadBtnClicked(_ sender: Any) {
+        let storage = Storage.storage()
+        let storageReference = storage.reference()
+        
+        let mediaFolder = storageReference.child("media") //you can do this even not creating media folder at first on firebaseStorage
+        
+        if let data = imageView.image?.jpegData(compressionQuality: 0.5) {
+            
+            let imageReference = mediaFolder.child("image.jpg")
+            imageReference.putData(data, metadata: nil) { metaData , error in
+                if error != nil {
+                    print(error?.localizedDescription)
+                }else {
+                    
+                    imageReference.downloadURL { url, error in
+                        
+                        if error == nil {
+                            let imageURL = url?.absoluteString
+                            print(imageURL)
+                        }
+                        
+                    }
+                }
+            }
+        }
+        
+    }
+    
 }
